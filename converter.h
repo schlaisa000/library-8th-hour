039ead44040bc8a7063969768abaabd3d644657d
schlaisa000 <31670150+schlaisa000@users.noreply.github.com>
2017-11-02 15:04:18 -0500

    Update converter.h

--- converter.h
+++ converter.h
@@ -6,3 +6,19 @@ float cm_to_in(void)
     printf("\nThe number of inches is %.2f\n", in);
     return in;
 }
+float km_to_in(void)
+{
+    // Created by: Isaiah Schlagel
+    float km = get_float( "Kilometers: ");
+    float in = km * 39370.1;
+    printf( "\nThe number of inches is %.2f\n", in );
+    return in;
+}
+float km_to_feet(void)
+{
+    // Created by: Isaiah Schlagel
+    float km = get_float( "Kilometers: " );
+    float feet = km * 3280.841666667;
+    printf( "\nThe number of feet is %.2f\n", feet );
+    return feet;
+}
