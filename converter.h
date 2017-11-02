a51cc9e413fddcc27490c106a78f651dac31d8ec
bartholomewt <bartholomewt@bdusd.org>
2017-11-02 15:03:03 -0500

    Create converter.h

---
+++ converter.h
@@ -0,0 +1,8 @@
+float cm_to_in(void)
+{
+    //TUCKER BARTHOLOMEW
+    float cm = get_float("Number of centimeters:");
+    float in = cm / 2.54;
+    printf("\nThe number of inches is %.2f\n", in);
+    return in;
+}
